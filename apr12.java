acf5abc8444d220ea4a23c2b71077961b185357d
Strangedip <sg02295@gmail.com>
2023-04-14 19:23:36 +0530

    updated

---
+++ apr12.java
@@ -0,0 +1,17 @@
+public class apr12 {
+
+// TERNARY OPERATORS
+    // Conditional Operator
+    // condition ? if true : if false
+    // cond ? val1 : val2
+    public static void main(String[] args){
+    int a=5;
+    int b=7;
+    System.out.println(a>b?a:b);
+
+    //absolute no. always positive
+    int n= -2;
+    System.out.println(n>0?n:n*(-1));
+
+    }
+}
---
+++ apr13.java
@@ -0,0 +1,5 @@
+public class apr13 {
+// Decision Making Statement
+    // 1) simple if
+    
+}
