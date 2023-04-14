be7efd00aa1428dcc135d71f746f893adbef0620
Strangedip <sg02295@gmail.com>
2023-04-14 20:45:42 +0530

    updated

--- apr13.java
+++ apr13.java
@@ -1,5 +1,39 @@
 public class apr13 {
-// Decision Making Statement
-    // 1) simple if
-    
+    public static void main(String[] args) {
+        // Decision Making Statement
+        // 1) simple if
+        int a = 12;
+        if (a % 2 == 0) {
+            System.out.println("even");
+        }
+
+        // 2) if else
+        if (a % 2 == 0) {
+            System.out.println("even");
+        } else {
+            System.out.println("odd");
+        }
+
+        // 3) ladder if else if
+        if (a == 1) {
+            System.out.println("one");
+        } else if (a == 2) {
+            System.out.println("two");
+        } else if (a == 3) {
+            System.out.println("three");
+        } else {
+            System.out.println("unknown");
+        }
+
+        // 4) Nested Decision making statements
+        if (a % 2 == 0) {
+            if (a == 2) {
+                System.out.println("even two");
+            } else {
+                System.out.println("even");
+            }
+        } else {
+            System.out.println("odd");
+        }
+    }
 }
---
+++ apr14.java
@@ -0,0 +1,44 @@
+public class apr14 {
+    public static void main(String[] args) {
+
+        // 5) Switch DMS
+        int n = 2;
+        switch (n) {
+            case 1:
+                System.out.println("one");
+                break;
+
+            case 2:
+                System.out.println("two");
+                break;
+
+            case 3:
+                System.out.println("three");
+                break;
+
+            default:
+                System.out.println("default");
+                break;
+        }
+
+        // LOOPS
+        // 1) while loop
+        while (n < 5) // condition
+        {
+            System.out.println(n); // statement
+            n++; // updation
+        }
+
+        // 2) Do while loop
+        do {
+            System.out.println(n);
+            n++;
+        } while (n < 10);
+
+        // 3) for loop
+        for (int i = 0; i < 5; i++) {
+            System.out.println(i);
+        }
+
+    }
+}
