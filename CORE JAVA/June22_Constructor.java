3ce8341412b6f2f533d6c8018c8ee66da43b3d07
Strangedip <sg02295@gmail.com>
2023-06-24 00:47:37 +0530

    updated

--- CORE JAVA/June22_Constructor.java
+++ CORE JAVA/June22_Constructor.java
@@ -2,34 +2,41 @@
 
 public class June22_Constructor {
     public static void main(String[] args) {
-        //creating objects from student class
-        Student sandip=new Student(100, "Sandip Gupta", 75);
-        Student gulshan=new Student(101, "Gulshan Gupta", 95);
-        Student aarohi=new Student(102, "Aarohi Gupta", 85);
+        // creating objects from student class
+        Student sandip = new Student(100, "Sandip Gupta", 75);
+        Student gulshan = new Student(101, "Gulshan Gupta", 95);
+        Student aarohi = new Student(102, "Aarohi Gupta", 85);
 
-        //printing details
-        System.out.println( sandip.name + ", ID:" + sandip.id +", Marks:"+ sandip.marks);
-        System.out.println( gulshan.name + ", ID:" + gulshan.id +", Marks:"+ gulshan.marks);
-        System.out.println( aarohi.name + ", ID:" + aarohi.id +", Marks:"+ aarohi.marks);
+        // printing details
+        sandip.det();
+        gulshan.det();
+        aarohi.det();
     }
 
 }
+
 // creted new student class
 // ** another byte code file will be created for student class
-class Student{
-    //non static variable declared
+class Student {
+    // non static variable declared
     int id;
     String name;
     double marks;
 
-    //Constructor for student class
-    Student(int id,String name,double marks){
-        this.id=id;
-        this.name=name;
-        this.marks=marks;
+    // Constructor for student class
+    Student(int id, String name, double marks) {
+        this.id = id;
+        this.name = name;
+        this.marks = marks;
     }
+
+    // printing details
+    void det() {
+        System.out.println(this.name + ", ID:" + this.id + ", Marks:" + this.marks);
+    }
+
     public static void main(String[] args) {
-        
+
     }
 
 }
