2f37ef8df293901f68f2eb0e2bb178fe3874166a
Strangedip <sg02295@gmail.com>
2023-06-26 23:54:58 +0530

    updated

--- CORE JAVA/Test.java
+++ CORE JAVA/Test.java
@@ -2,21 +2,26 @@
 public class Test {
 
     // independet (static) class inside another class
-    static class Human{  
-    static int eyeCount=12;  // static variable for Human class
+    static class Human {
+        static int eyeCount = 2; // static variable for Human class
     }
 
-    static int eyeCount=3;  //static variable for Test class
+    class Human1 {
+        int eyeCount = 3; // static variable for Human class
+    }
+
+    static int eyeCount = 0; // static variable for Test class
     int sal = 20;
 
     // Test class main method
     public static void main(String[] args) {
-        // Test obj=new Test();
-        // Test.Human obj1= new Human();
-        System.out.println(Human.eyeCount);  //static members of Human class (in Human class static area)  
-        System.out.println(Test.eyeCount);   //static members of Test class (in Test class static area)
-        
+
+        System.out.println(Human.eyeCount); // static members of Human class (in Human class static area)
+        System.out.println(Test.eyeCount); // static members of Test class (in Test class static area)
+
+        Test obj = new Test(); // creating outer non static class object to use it
+        Test.Human obj2 = new Human(); // creating inner static class object to use it
+        Test.Human1 obj1 = obj.new Human1(); // creating inner non static class object to use it
+        System.out.println(obj1.eyeCount);
     }
 }
-
-
