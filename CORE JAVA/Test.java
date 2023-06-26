267fb0e35eb6f85f091c2ac1cb78e3593fa65434
Strangedip <sg02295@gmail.com>
2023-06-26 23:19:53 +0530

    updated

--- CORE JAVA/Test.java
+++ CORE JAVA/Test.java
@@ -1,16 +1,17 @@
 
 public class Test {
-    static class Human{
-    static int eyeCount=12;
+    static class Human{  
+    static int eyeCount=12;  // eye for Human class
     }
 
-    static int eyeCount=3;
+    static int eyeCount=3;  //eye for Test class
     int sal = 20;
     public static void main(String[] args) {
-        Test obj=new Test();
-        Test.Human obj1= new Human();
-        System.out.println(Human.eyeCount);  //use static members by using their class name  
-        System.out.println(Test.eyeCount);    
+        // Test obj=new Test();
+        // Test.Human obj1= new Human();
+        System.out.println(Human.eyeCount);  //use static members by using THEIR class name  
+        System.out.println(Test.eyeCount);  
+        
     }
 }
 
