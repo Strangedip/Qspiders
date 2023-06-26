172fa43654d0fb55bbdf8ab71bce6f3c567eafed
Strangedip <sg02295@gmail.com>
2023-06-26 23:37:13 +0530

    updated

--- CORE JAVA/Test.java
+++ CORE JAVA/Test.java
@@ -1,16 +1,20 @@
-
+//dependent non static class 
 public class Test {
+
+    // independet (static) class inside another class
     static class Human{  
-    static int eyeCount=12;  // eye for Human class
+    static int eyeCount=12;  // static variable for Human class
     }
 
-    static int eyeCount=3;  //eye for Test class
+    static int eyeCount=3;  //static variable for Test class
     int sal = 20;
+
+    // Test class main method
     public static void main(String[] args) {
         // Test obj=new Test();
         // Test.Human obj1= new Human();
-        System.out.println(Human.eyeCount);  //use static members by using THEIR class name  
-        System.out.println(Test.eyeCount);  
+        System.out.println(Human.eyeCount);  //static members of Human class (in Human class static area)  
+        System.out.println(Test.eyeCount);   //static members of Test class (in Test class static area)
         
     }
 }
