8ce6b627e2c8899f0ccfe06d86a13a268886937d
Strangedip <sg02295@gmail.com>
2023-08-02 15:33:09 +0530

    jdk 1.8 interface

---
+++ programming/Jdk8.java
@@ -0,0 +1,29 @@
+// normal interface
+interface I{
+    // can have variables inside interface auto prefixed with 'final static'
+    final static int var=10;
+
+    //can create concrete method inside interface prefixed with 'default'
+    default void method1(){}
+
+    //abstract method
+    void method2();
+    //abstract method
+    void method3();
+}
+
+// marker interface // no methods at all // used for jvm indications
+interface I2{}
+
+//Functional interface  // only one abstract method
+@FunctionalInterface
+interface I3{
+    void add();
+}
+
+public class Jdk8{
+    public static void main(String[] args) {
+        
+    }
+
+}
